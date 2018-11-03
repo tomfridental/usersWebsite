0a35dfe9fffbd64ff352c3e49650b7ad1d135484
tomfridental <tomfridental@gmail.com>
2018-11-03 12:03:12 +0200

    test2

--- src/components/Yossi.js
+++ src/components/Yossi.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({})=> {
     return(
         <div>
-            <h1>Under Construction!</h1>
+            <h1>Under Construction!, Coming Soon</h1>
         </div>
     )   
 }
